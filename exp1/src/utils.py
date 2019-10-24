9d06d3aedd7ef417e2c87be294c45aa8c01e83ae
apoorvagni@kubuntu <apoorvagni@hotmail.com>
2019-10-24 18:55:53 +0530

    exp1 init

---
+++ exp1/src/utils.py
@@ -0,0 +1,9 @@
+import numpy as np
+from sklearn.metrics import mean_absolute_error, mean_squared_error
+
+def rmse(ypred, y):
+    mse = mean_squared_error(np.array(ypred), np.array(ypred))
+    return np.sqrt(mse)
+
+def mae(ypred, y):
+    return mean_absolute_error(np.array(ypred), np.array(ypred))
