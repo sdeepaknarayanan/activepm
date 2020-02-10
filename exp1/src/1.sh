05eb712bcecc865641a8b905ffe631b382d23b8c
apoorvagni@kubuntu <apoorvagni@hotmail.com>
2020-02-11 00:22:14 +0530

    added more scripts

--- exp1/src/1.sh
+++ exp1/src/1.sh
@@ -1,4 +1,3 @@
-python3 called.py --reg gpFULL --stepSize 2 --lastKDays 1 --datafile ../../data/beijingb_scaled.csv --loc results -s
 python3 called.py --reg gpFULL --stepSize 2 --lastKDays 2 --datafile ../../data/beijingb_scaled.csv --loc results -s
 python3 called.py --reg gpFULL --stepSize 2 --lastKDays 3 --datafile ../../data/beijingb_scaled.csv --loc results -s
 python3 called.py --reg gpFULL --stepSize 2 --lastKDays 4 --datafile ../../data/beijingb_scaled.csv --loc results -s
@@ -7,9 +6,4 @@ python3 called.py --reg gpFULL --stepSize 2 --lastKDays 6 --datafile ../../data/
 python3 called.py --reg gpFULL --stepSize 2 --lastKDays 7 --datafile ../../data/beijingb_scaled.csv --loc results -s
 python3 called.py --reg gpFULL --stepSize 2 --lastKDays 8 --datafile ../../data/beijingb_scaled.csv --loc results -s
 python3 called.py --reg gpFULL --stepSize 2 --lastKDays 9 --datafile ../../data/beijingb_scaled.csv --loc results -s
-python3 called.py --reg gpFULL --stepSize 2 --lastKDays 10 --datafile ../../data/beijingb_scaled.csv --loc results -s
-python3 called.py --reg gpFULL2 --stepSize 2 --lastKDays 1 --datafile ../../data/beijingb_scaled.csv --loc results -s
 python3 called.py --reg gpFULL2 --stepSize 2 --lastKDays 2 --datafile ../../data/beijingb_scaled.csv --loc results -s
-python3 called.py --reg gpFULL2 --stepSize 2 --lastKDays 3 --datafile ../../data/beijingb_scaled.csv --loc results -s
-python3 called.py --reg gpFULL2 --stepSize 2 --lastKDays 4 --datafile ../../data/beijingb_scaled.csv --loc results -s
-python3 called.py --reg gpFULL2 --stepSize 2 --lastKDays 5 --datafile ../../data/beijingb_scaled.csv --loc results -s
--- exp1/src/4.sh
+++ exp1/src/4.sh
@@ -1,4 +1,3 @@
-python3 called.py --reg gpST --stepSize 2 --lastKDays 1 --datafile ../../data/beijinga_scaled.csv --loc results -s
 python3 called.py --reg gpST --stepSize 2 --lastKDays 2 --datafile ../../data/beijinga_scaled.csv --loc results -s
 python3 called.py --reg gpST --stepSize 2 --lastKDays 3 --datafile ../../data/beijinga_scaled.csv --loc results -s
 python3 called.py --reg gpST --stepSize 2 --lastKDays 4 --datafile ../../data/beijinga_scaled.csv --loc results -s
@@ -7,8 +6,4 @@ python3 called.py --reg gpST --stepSize 2 --lastKDays 6 --datafile ../../data/be
 python3 called.py --reg gpST --stepSize 2 --lastKDays 7 --datafile ../../data/beijinga_scaled.csv --loc results -s
 python3 called.py --reg gpST --stepSize 2 --lastKDays 8 --datafile ../../data/beijinga_scaled.csv --loc results -s
 python3 called.py --reg gpST --stepSize 2 --lastKDays 9 --datafile ../../data/beijinga_scaled.csv --loc results -s
-python3 called.py --reg gpST --stepSize 2 --lastKDays 10 --datafile ../../data/beijinga_scaled.csv --loc results -s
-python3 called.py --reg gpST2 --stepSize 2 --lastKDays 1 --datafile ../../data/beijinga_scaled.csv --loc results -s
 python3 called.py --reg gpST2 --stepSize 2 --lastKDays 2 --datafile ../../data/beijinga_scaled.csv --loc results -s
-python3 called.py --reg gpST2 --stepSize 2 --lastKDays 3 --datafile ../../data/beijinga_scaled.csv --loc results -s
-python3 called.py --reg gpST2 --stepSize 2 --lastKDays 4 --datafile ../../data/beijinga_scaled.csv --loc results -s
